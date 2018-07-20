c27a27aebd0050950bb551aaefa90e11b28dc566
belaldaqqah <belaldaqqah@rfs.edu.ps>
2018-07-20 15:04:44 +0300

    Add Firebase to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { firebaseConfig } from '../config';
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,7 +19,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -23,6 +30,8 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    AngularFireAuth,
+    AngularFireDatabase,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBCWHQpWJNyH89qNEzh8UKDs_L9rL0-yac",
+    authDomain: "shoppinglistapp-25.firebaseapp.com",
+    databaseURL: "https://shoppinglistapp-25.firebaseio.com",
+    projectId: "shoppinglistapp-25",
+    storageBucket: "shoppinglistapp-25.appspot.com",
+    messagingSenderId: "442443397379"
+};
