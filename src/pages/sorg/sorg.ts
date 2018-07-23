8c5714a75d20192597a499a6480b3c879c50c2b4
Omar Khatib <omarkhatib@rfs.edu.ps>
2018-07-23 10:55:04 +0300

    Fix capitalization

--- src/pages/sorg/sorg.ts
+++ src/pages/sorg/sorg.ts
@@ -1,4 +1,4 @@
-import { RegisterPage } from './../RegisterStudent/RegisterStudent';
+import { RegisterPage } from './../registerStudent/registerStudent';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
